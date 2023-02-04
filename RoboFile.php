0467e78c9ee4130abfb6c671586fff8109870104
Easy <easychen@qq.com>
2023-02-04 17:34:29 +0800

    publish

--- RoboFile.php
+++ RoboFile.php
@@ -7,8 +7,8 @@
 class RoboFile extends \Robo\Tasks
 {
     // define public methods as commands
-    public function publish()
+    public function publish($m = 'publish')
     {
-        $this->_exec("yarn build && yarn package && git add . && git commit -m 'publish' && git push ");
+        $this->_exec("yarn build && yarn package && git add . && git commit -m '$m' && git push ");
     }
 }
