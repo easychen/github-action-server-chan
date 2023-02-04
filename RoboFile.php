ab4a36126c4dce313f630e19ea1a5bac848657bd
Easy <easychen@qq.com>
2023-02-04 17:31:24 +0800

    publish

---
+++ RoboFile.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see https://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+    // define public methods as commands
+    public function publish()
+    {
+        $this->_exec("yarn build && yarn package && git add . && git commit -m 'publish' && git push ");
+    }
+}
